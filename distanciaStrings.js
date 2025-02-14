133d28c3b1bb5a1751c8ad2165d1bce694077081
chaira55 <sharkcar55@gmail.com>
2025-02-14 15:25:06 -0500

    Ejercicio 31 terminado, todos los ejercicios estan completos

--- distanciaStrings.js
+++ distanciaStrings.js
@@ -1,3 +1,16 @@
 function distancia(string1, string2){
+    let diferencia = 0;
+
+    for(let i = 0; i < string1.length; i++){
+        if(string1[i] != string2[i]){
+            diferencia += 1;
+        }
+    }
+
+    return diferencia;
     
 }
+
+console.log(distancia("hola", "hola")) // 0 
+console.log(distancia("sol", "tol")) // 1 
+console.log(distancia("carro", "correr")) // 3 
